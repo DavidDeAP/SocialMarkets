71fa9ca71bb677e3890d370d3276db8d687a9053
David de Antonio Palomar <david.de89@educa.madrid.org>
2026-04-19 13:54:21 +0200

    feat: configurada navegacion con react-router-dom y rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Registro from './pages/Registro'; // Importar interfaz
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Registro from './pages/Registro';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Registro />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* La página principal será el Login */}
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          
+          {/* La página de registro */}
+          <Route path="/registro" element={<Registro />} />
+
+          {/* Si alguien escribe cualquier otra cosa, lo mandamos al login */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
