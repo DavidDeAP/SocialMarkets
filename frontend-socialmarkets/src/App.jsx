22baad92bb926bcefd74464b691762933f4c968c
David de Antonio Palomar <david.de89@educa.madrid.org>
2026-04-21 21:10:17 +0200

    style: mejora del dashboard con fondo desenfocado y unión del fondo en todas las clases

--- frontend-socialmarkets/src/App.jsx
+++ frontend-socialmarkets/src/App.jsx
@@ -9,17 +9,10 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          {/* La página principal será el Login */}
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
-          
-          {/* La página de registro */}
           <Route path="/registro" element={<Registro />} />
-
-          {/* Pagina home de prueba */}
           <Route path="/home" element={<Home />} />
-
-          {/* Si alguien escribe cualquier otra cosa, lo mandamos al login */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
